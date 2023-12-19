da45aa05dbf60672fb36868a84c29f513688bf1a
hayamaster <dnfxmfkdbzos@naver.com>
2023-12-19 20:07:28 +0900

    feat: sidebar 클릭시 맵 중앙 이동

--- src/pages/MapPage/components/SideBar/index.tsx
+++ src/pages/MapPage/components/SideBar/index.tsx
@@ -1,4 +1,5 @@
 import { MouseEvent, Dispatch, SetStateAction } from 'react'
+import { useMap } from '@vis.gl/react-google-maps'
 
 interface Position {
   lat: number
@@ -19,34 +20,43 @@ interface MarkerInformationProps {
 }
 
 const SideBar = ({ markerInformation, setFocusId }: MarkerInformationProps) => {
+  const map = useMap()
+
   const handleFocusInfo = (e: MouseEvent<HTMLDivElement>) => {
     e.preventDefault()
     setFocusId(Number(e.currentTarget.id))
   }
 
+  const handleClickInfo = (position: Position) => {
+    map?.panTo(position)
+  }
+
   return (
-    <div className="fixed right-6 top-6 z-10 flex flex-col gap-2.5">
-      {markerInformation.map(({ id, name, location, description }) => (
-        <div
-          key={id}
-          onMouseOver={handleFocusInfo}
-          onMouseLeave={() => setFocusId(0)}
-          id={String(id)}
-          className="group flex flex-col justify-start gap-2 rounded-2xl border border-[rgb(151,71,255)] bg-white px-3 py-2 shadow-xl hover:bg-[rgb(151,71,255)] hover:duration-200 md:px-4 md:py-3"
-        >
-          <h1 className="cursor-pointer text-base text-[#9747FF] group-hover:text-white md:text-2xl">
-            {name}
-          </h1>
-          <p className="cursor-default text-sm text-gray-400 group-hover:text-gray-300 md:text-lg">
-            {location && location.length > 18
-              ? `${location?.substring(0, 18)}...`
-              : location}
-          </p>
-          <p className="cursor-default whitespace-pre-wrap text-sm text-gray-700 group-hover:text-gray-100 md:text-lg">
-            {description}
-          </p>
-        </div>
-      ))}
+    <div className="fixed right-6 top-6 z-10 flex cursor-pointer flex-col gap-2.5">
+      {markerInformation.map(
+        ({ id, name, location, description, position }) => (
+          <div
+            key={id}
+            onMouseOver={handleFocusInfo}
+            onMouseLeave={() => setFocusId(0)}
+            onClick={() => handleClickInfo(position)}
+            id={String(id)}
+            className="group flex flex-col justify-start gap-2 rounded-2xl border border-[rgb(151,71,255)] bg-white px-3 py-2 shadow-xl hover:bg-[rgb(151,71,255)] hover:duration-200 md:px-4 md:py-3"
+          >
+            <h1 className="text-base text-[#9747FF] group-hover:text-white md:text-2xl">
+              {name}
+            </h1>
+            <p className="text-sm text-gray-400 group-hover:text-gray-300 md:text-lg">
+              {location && location.length > 18
+                ? `${location?.substring(0, 18)}...`
+                : location}
+            </p>
+            <p className="whitespace-pre-wrap text-sm text-gray-700 group-hover:text-gray-100 md:text-lg">
+              {description}
+            </p>
+          </div>
+        ),
+      )}
     </div>
   )
 }
