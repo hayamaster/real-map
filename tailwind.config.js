8445e70bc70aa253c1c108ede6d5f4eb8ac24b4f
hayamaster <dnfxmfkdbzos@naver.com>
2023-12-19 19:54:28 +0900

    chore: tailwind font size 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontSize: {
+      xs: '0.625rem',
+      sm: '0.75rem',
+      base: '0.8125rem',
+      lg: '0.875rem',
+      xl: '1rem',
+      '2xl': '1.125rem',
+      '3xl': '1.5rem',
+      '4xl': '1.875rem',
+    },
   },
   plugins: [],
 }
-
