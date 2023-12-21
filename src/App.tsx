b6eb7d3c73428e98be9b5f9fa5de076758758c35
hayamaster <dnfxmfkdbzos@naver.com>
2023-12-21 17:26:46 +0900

    refactor: ga를 ga4로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
-import { initialize, pageview } from 'react-ga'
+import ReactGA from 'react-ga4'
 import { useEffect } from 'react'
 
 const gaTrackingId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID // 환경 변수에 저장된 추적ID 가져오기
-initialize(gaTrackingId, { debug: true }) // react-ga 초기화 및 debug 사용
+ReactGA.initialize(gaTrackingId) // react-ga 초기화 및 debug 사용
 
 function App() {
   useEffect(() => {
-    pageview(window.location.pathname + window.location.search)
+    ReactGA.set({ page: location.pathname })
+    ReactGA.send('pageview')
   }, [])
 
   return (
