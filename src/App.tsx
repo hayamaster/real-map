eedc2de119c6df89037fed4d1a4380a70d69e0e5
hayamaster <dnfxmfkdbzos@naver.com>
2023-12-21 17:03:59 +0900

    feat ga 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { initialize, pageview } from 'react-ga'
+import { useEffect } from 'react'
+
+const gaTrackingId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID // 환경 변수에 저장된 추적ID 가져오기
+initialize(gaTrackingId, { debug: true }) // react-ga 초기화 및 debug 사용
 
 function App() {
+  useEffect(() => {
+    pageview(window.location.pathname + window.location.search)
+  }, [])
+
   return (
     <div className="h-screen">
       <RouterProvider router={router} />
