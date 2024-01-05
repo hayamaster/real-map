b02533b95f2fb8e55fe321375a1074079c879a65
hayamaster <dnfxmfkdbzos@naver.com>
2024-01-06 01:42:53 +0900

    feat: ShareIcon 추가

--- src/assets/Icons/index.ts
+++ src/assets/Icons/index.ts
@@ -1,3 +1,4 @@
 export { default as MarkerIcon } from './marker.svg?react'
 export { default as CopyIcon } from './copy.svg?react'
 export { default as CheckIcon } from './check.svg?react'
+export { default as ShareIcon } from './share.svg?react'
